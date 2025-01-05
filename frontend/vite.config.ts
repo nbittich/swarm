d137f11c2f6fa84935b7e92df47a1c2a5e7918c7
Nordine Bittich <contact@bittich.be>
2025-01-05 18:31:45 +0100

    use nginx as a proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,31 +2,31 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-redux', 'cron-validate', 'uuid', 'react-router-dom', '@reduxjs/toolkit'],
-          ui: ['@ant-design/colors', 'antd'],
-          yasgui: ['@triply/yasgui']
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ['react', 'react-dom', 'react-redux', 'cron-validate', 'uuid', 'react-router-dom', '@reduxjs/toolkit'],
+                    ui: ['@ant-design/colors', 'antd'],
+                    yasgui: ['@triply/yasgui']
+                }
+            }
         }
-      }
-    }
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@swarm": "/src",
     },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@swarm": "/src",
+        },
     },
-  },
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                secure: false,
+                // rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+        },
+    },
 })
